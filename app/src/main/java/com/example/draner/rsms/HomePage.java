2440bf592d8a6086650cd9dd5478e46b5955814d
dranercom <ronythankachan324@gmail.com>
2018-04-07 17:48:17 +0530

    fixed login,webview of user in dashboard,logout

--- app/src/main/java/com/example/draner/rsms/HomePage.java
+++ app/src/main/java/com/example/draner/rsms/HomePage.java
@@ -1,13 +1,12 @@
 package com.example.draner.rsms;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
+import android.os.Handler;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.view.KeyEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,17 +14,28 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
 
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EncodingUtils;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
+
+import static com.example.draner.rsms.MainActivity.editor;
 
 public class HomePage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -34,16 +44,16 @@ public class HomePage extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView =findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         //set username on navigation drawer
@@ -53,18 +63,83 @@ public class HomePage extends AppCompatActivity
 
         //set profile picture in navigation drawer
         View header = navigationView.getHeaderView(0);
-        ImageView imageView = (ImageView) header.findViewById(R.id.profilePic);
+        ImageView imageView = header.findViewById(R.id.profilePic);
         String profilepic_url="https://www.rajagiritech.ac.in/stud/Photo/"+bundle.getString("username")+".jpg";
-
         Glide
                 .with(this)
                 .load(profilepic_url)
                 .placeholder(R.drawable.ic_person_outline_black_24dp)
                 .error(R.drawable.ic_person_outline_black_24dp)
                 .into(imageView);
-    }
+        //load rsms to dashboard
+        String data="user="+bundle.getString("username")+"&pass="+bundle.getString("password")+"&I1.x=0&I2.y=0";
+        WebView webView = findViewById(R.id.webview);
+        WebViewClient wvc = new WebViewClient() {
+            @Override
+            public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
 
+                try {
+                    DefaultHttpClient client = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet(url);
+                    httpGet.setHeader("Content-Type", "application/x-www-form-urlencoded");
+                    HttpResponse httpResponse = client.execute(httpGet);
+
+                    Header contentType = httpResponse.getEntity().getContentType();
+                    Header encoding = httpResponse.getEntity().getContentEncoding();
+                    InputStream responseInputStream = httpResponse.getEntity().getContent();
+
+                    String contentTypeValue = null;
+                    String encodingValue = null;
+                    if (contentType != null) {
+                        contentTypeValue = contentType.getValue();
+                    }
+                    if (encoding != null) {
+                        encodingValue = encoding.getValue();
+                    }
+                    return new WebResourceResponse(contentTypeValue, encodingValue, responseInputStream);
+                } catch (ClientProtocolException e) {
+                    //return null to tell WebView we failed to fetch it WebView should try again.
+                    return null;
+                } catch (IOException e) {
+                    //return null to tell WebView we failed to fetch it WebView should try again.
+                    return null;
+                }
+            }
+        };
+        webView.setWebViewClient(wvc);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        //To go back to the previous page when back button is pressed
+        webView.setOnKeyListener(new View.OnKeyListener()
+        {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if(event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode)
+                    {
+                        case KeyEvent.KEYCODE_BACK:
+                            if(webView.canGoBack())
+                            {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+        webView.postUrl("https://www.rajagiritech.ac.in/stud/parent/varify.asp", EncodingUtils.getBytes(data,"BASE64"));
+    }
     @Override
+
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -86,6 +161,7 @@ public class HomePage extends AppCompatActivity
         } else if (id == R.id.scores) {
 
         } else if (id == R.id.seating_plan) {
+            
 
         } else if (id == R.id.notice) {
 
@@ -94,10 +170,15 @@ public class HomePage extends AppCompatActivity
         } else if (id == R.id.about_developer) {
 
         } else if (id == R.id.logout) {
-
+            editor.clear();
+            editor.commit();
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
--- app/src/main/java/com/example/draner/rsms/MainActivity.java
+++ app/src/main/java/com/example/draner/rsms/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.draner.rsms;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public SharedPreferences sp;
+    public static SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button =findViewById(R.id.login_button);
+        //To go to home page if already logged in
+        sp=getSharedPreferences("login",MODE_PRIVATE);
+        editor=sp.edit();
+        if(sp.getString("username",null)!=null && sp.getString("password",null)!=null){
+            Intent intent=new Intent(this,HomePage.class);
+            intent.putExtra("username",sp.getString("username",null));
+            intent.putExtra("password",sp.getString("password",null));
+            startActivity(intent);
+            finish();
+        }
         button.setOnClickListener(this);
     }
     @Override
@@ -20,11 +36,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText password_form=findViewById(R.id.password_form);
         String uname=uid_form.getText().toString();
         String pword=password_form.getText().toString();
+        editor.putString("username",uname);
+        editor.putString("password",pword);
+        editor.commit();
         Intent intent=new Intent(getApplicationContext(),HomePage.class);
         intent.putExtra("username",uname);
         intent.putExtra("password",pword);
         startActivity(intent);
-        setContentView(R.layout.activity_home_page);
+        finish();
+    }
+    private Boolean exit = false;
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+
     }
 }
 
